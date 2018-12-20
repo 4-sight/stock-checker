3bfc703c8ee155722078d17eae347564e55c1e79
Peter Newton <peter@4sightdesigns.com>
2018-12-20 15:50:31 +0000

    add methods to stocks.js

--- models/stocks.js
+++ models/stocks.js
@@ -6,5 +6,32 @@ const stockSchema = new mongoose.Schema({
   likes: Number
 })
 
-module.exports = mongoose.model('StockPrice-stocks', stockSchema)
+const StockDB = mongoose.model('StockPrice-stocks', stockSchema)
 
+//Methods
+
+StockDB.addLikeAndReturn = async function (stock) {
+
+  let result = await this.findOneAndUpdate(
+    { stock: stock },
+    { $inc: { likes: 1 }},
+    {
+      upsert: true,
+      new: true
+    },
+  ).then(result => result._doc.likes)
+  
+  return result
+}
+
+StockDB.getLikesAndReturn = async function (stock) {
+
+  let numLikes = await this.findOne({ stock: stock.symbol })
+    .then(res => {
+      if(res) {return res._doc.likes}
+    })
+  return numLikes
+}
+
+
+module.exports = StockDB
